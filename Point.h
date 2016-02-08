0eb1f05b1eba1820ccdfa699440c1a4d6dd34ccd
heavenphenix <david.t.lam@ucdenver.edu>
2016-02-07 21:52:55 -0700

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,5 +1,8 @@
 // A 2-dimensional point class!
 // Coordinates are double-precision floating point.
+// by David Lam
+// editted 2.7.16
+
 
 #ifndef __POINT_H
 #define __POINT_H
@@ -7,21 +10,27 @@
 class Point {
     
 private:
-    double x, y;
+    double x, y,z;
+// this makes this into a 3 dimensional point
 
 public:
     // Constructors
     Point();                      // default constructor
-    Point(double x, double y);    // two-argument constructor
+    Point(double x, double y, double z);    // three argument constructor
 
     // Mutator methods
     void setX(double newX);
     void setY(double newY);
-
+    void setZ(double newZ);
+    // add in the new dimension mutator
+    
     // Accessor methods
     double getX() const;
     double getY() const;
+    double getZ() const;
     
+    // calls the distance back between the two points
+    double distanceTo(const Point &) const;
 };
 
 #endif // __POINT_H
